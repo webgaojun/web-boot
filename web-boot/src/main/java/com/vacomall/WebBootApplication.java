dcaa43ffd9b013e0649e4e82d8014d6596175982
gaojun.zhou <274410087@qq.com>
2017-01-20 14:25:05 +0800

    Change-Id: Ie100d0b07c9f99cb511130a347f7ef4cf9bada29

---
+++ web-boot/src/main/java/com/vacomall/WebBootApplication.java
@@ -0,0 +1,12 @@
+package com.vacomall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebBootApplication.class, args);
+	}
+}
